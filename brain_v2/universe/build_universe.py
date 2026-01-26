dde5f53a04ad11d08848a89d93b34dee90902415
coohen26-gif <coohen26@gmail.com>
2026-01-27 01:32:03 +0200

    Update line 15 to remove config reference in build_universe.py

--- brain_v2/universe/build_universe.py
+++ brain_v2/universe/build_universe.py
@@ -1,17 +1,5 @@
-# Updated build_universe.py
+# Updated line 15 in brain_v2/universe/build_universe.py
 
-# Import statements
-from brain_v2.universe.bitget_client import fetch_usdt_perpetual_markets
+bitget_symbols = fetch_usdt_perpetual_markets()  
 
-# Other parts of the code remain unchanged...
-
-# Update the section instantiating the bitget_client
-# Removed the BitgetClient instantiation
-# bitget_client = BitgetClient(base_url=config["bitget_api_base_url"])
-# Removed method call
-# bitget_symbols = bitget_client.fetch_usdt_perpetual_markets()
-
-# Updated the call to use the functional API directly
-bitget_symbols = fetch_usdt_perpetual_markets(base_url=config["bitget_api_base_url"])
-
-# Continue with the rest of the code...
+# This removes the undefined config reference and uses the default base_url parameter.
